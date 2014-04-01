9fdd3936240225c8834c223abb0f74d74122a927
MBO <mbo@octo.com>
2014-04-01 14:21:29 +0200

    Commit to described how these data have be cleaned from original version of NewSQL. These comment will be suppressed in following commits.

--- perfdummies/app/src/main/resources/db/pgsql/import.sql
+++
@@ -1,7 +0,0 @@
-\copy STORE  FROM '../../../../../data/small/Store.csv' DELIMITER ',' CSV;a
-\copy CATEGORYFAMILY FROM '../../../../../data/small/CategoryFamily.csv' DELIMITER ',' CSV;
-\copy CATEGORYFAMILY_VAT FROM '../../../../../data/small/CategoryFamily_VAT.csv' DELIMITER ',' CSV;
-\copy PRODUCT  FROM '../../../../../data/small/Product.csv' DELIMITER ',' CSV;
-\copy STOCK  FROM '../../../../../data/small/Stock.csv' DELIMITER ',' CSV;
-\copy SALEOPERATION FROM '../../../../../data/small/SaleOperation.csv' DELIMITER ',' CSV;
-\copy SALETRANSACTION FROM '../../../../../data/small/SaleTransaction.csv' DELIMITER ',' CSV;
