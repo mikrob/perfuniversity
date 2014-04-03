01f772be09ce082f33eb87ee5a855c867f7a112d
Henri Tremblay <henri.tremblay@gmail.com>
2014-04-03 17:36:17 +0200

    Uneeded

--- happystore/deploy.sh
+++
@@ -1,6 +0,0 @@
-BASEDIR=$(dirname $0)
-TOMCAT_HOME=${BASEDIR}/tomcat
-APP_HOME=${BASEDIR}/app
-$BASEDIR/compile.sh package -Dmaven.test.skip=true
-cp $APP_HOME/target/happystore-1.0-SNAPSHOT.war $TOMCAT_HOME/webapps/happystore.war
-
