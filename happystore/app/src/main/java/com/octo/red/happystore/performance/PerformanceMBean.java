20de9e7534ded5b227feca2f86b3e10bacaf6724
Henri Tremblay <henri.tremblay@gmail.com>
2014-04-04 17:28:10 +0200

    Allow command line parameters to disable performance issues

--- happystore/app/src/main/java/com/octo/red/happystore/performance/PerformanceMBean.java
+++ happystore/app/src/main/java/com/octo/red/happystore/performance/PerformanceMBean.java
@@ -12,6 +12,11 @@ import org.springframework.stereotype.Component;
 @ManagedResource(objectName="bean:name=performance", description="Performance enabler")
 public class PerformanceMBean {
 
+    public static void init() {
+        StatFilter.enableLock = Boolean.parseBoolean(System.getProperty("statFilterEnabled", "true"));
+        CacheManager.enabled = Boolean.parseBoolean(System.getProperty("cacheEnabled", "true"));
+    }
+
     @ManagedAttribute(description="If the Stat filter (contention issue) is enabled")
     public boolean isStatFilterEnabled() {
         return StatFilter.enableLock;
