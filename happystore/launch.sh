492e8aa42fb4756bc250ae341727c7e07dc5fadc
MBO <mbo@octo.com>
2014-04-07 21:42:58 +0200

    remaining

--- happystore/launch.sh
+++ happystore/launch.sh
@@ -1,2 +1,2 @@
 BASEDIR=$(dirname $0)
-mvn -f ${BASEDIR}/app/pom.xml -Dmetrics.graphite.enabled=true -Dmetrics.graphite.port=8888 -Dmetrics.graphite.host=localhost -Dspring.profiles.active="h2" tomcat7:run
+mvn -f ${BASEDIR}/app/pom.xml -Dmetrics.graphite.enabled=true -Dmetrics.graphite.port=8888 -Dmetrics.graphite.host=localhost -Dspring.profiles.active="postgresql" tomcat7:run
