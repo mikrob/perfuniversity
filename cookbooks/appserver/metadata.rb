78f9623dad3460729121a5b44c219d717641785d
mikrob <mrobert@octo.com>
2014-04-10 18:44:50 +0200

    upgrade

---
+++ cookbooks/appserver/metadata.rb
@@ -0,0 +1,3 @@
+name "appserver"
+
+depends "collectd"
--- cookbooks/appserver/recipes/default.rb
+++ cookbooks/appserver/recipes/default.rb
@@ -1,3 +1,16 @@
 target_war = tomcat_instance "happystore:tomcat" do
   war_location "/happystore"
 end
+
+include_recipe "collectd"
+
+collectd_plugin "write_graphite" do
+ config <<-EOF
+<Carbon>
+        Host "192.168.56.11"
+        Port "2003"
+</Carbon>
+EOF
+end
+
+postgresql_database "happystore:database"
