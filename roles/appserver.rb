38cd873224090a76a444c0b420bdddbd4da535fb
mikrob <mrobert@octo.com>
2014-04-11 14:55:04 +0200

    db server role

--- roles/appserver.rb
+++ roles/appserver.rb
@@ -17,8 +17,13 @@ override_attributes({
           }
         },
         :env => {
-          "JAVA_OPTS" => "-XX:MaxPermSize=128m -Xmx512m -Xms128m -Dmetrics.graphite.enabled=true -Dmetrics.graphite.port=2003 -Dmetrics.graphite.host=192.168.56.11 -Dspring.profiles.active=\"h2\"",
+          "JAVA_OPTS" => "-XX:MaxPermSize=128m -Xmx512m -Xms128m -Dmetrics.graphite.enabled=true -Dmetrics.graphite.port=2003 -Dmetrics.graphite.host=192.168.56.11 -Dspring.profiles.active=\"postgresql\"",
         }
     },
-  }
+
+  },
+  :postgresql => {
+      :user_filters => ['0.0.0.0/32', '::1/128'],
+      :listen_addresses => '0.0.0.0',
+    }
 })
