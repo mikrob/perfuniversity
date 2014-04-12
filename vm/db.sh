12e161cf5eeb5cd563fb8d20c9108f45f0c61118
mbo <mbo@octo.com>
2014-04-12 22:20:57 +0200

    use offline-proxy

--- vm/db.sh
+++ vm/db.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+echo "Set offline-proxy"
+export http_proxy=http://192.168.56.1:3128
+
 echo "=> Updating apt"
 sudo apt-get update -y
 sudo DEBIAN_FRONTEND=noninteractive apt-get -y -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" dist-upgrade
